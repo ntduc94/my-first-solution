83f16719d45792af58eec6e0918a23f94002dffe
nguyen tan duc <ntanduc94@outlook.com>
2018-07-22 11:57:01 +0700

    Add project files.

---
+++ my-first-project/Controllers/HeroController.cs
@@ -0,0 +1,72 @@
+﻿using my_first_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_first_project.Controllers
+{
+    public class HeroController : Controller
+    {
+        // GET: Hero
+        public ActionResult Index()
+        {
+            return View("Contact");
+            //return View("~Views/Home/Index.cshtml");
+        }
+
+        public bool IsValid()
+        {
+            return false;
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public int HasPowerPoint()
+        {
+            return 100;
+        }
+
+        public RedirectToRouteResult ToHeroContact()
+        {
+            TempData["ToHeroContact"] = "To Hero Contact";
+            return RedirectToAction("Contact", "Hero");
+        }
+
+        public RedirectResult ToGooglePage()
+        {
+            return Redirect("https://google.com");
+        }
+
+        public ActionResult Detail(int Id)
+        {
+            Hero SelectedHero;
+            switch (Id)
+            {
+                case 1:
+                    SelectedHero = new Hero("Superman", 100);
+                    break;
+                case 2:
+                    SelectedHero = new Hero("Batman", 80);
+                    break;
+                default:
+                    SelectedHero = new Hero("TienNg", 0);
+                    break;
+            }
+
+            ViewData["FromTitle"] = "Hero Detail Form";
+            ViewData["CurrentTime"] = DateTime.Now;
+            ViewData["SelectedHero"] = SelectedHero;
+
+            ViewBag.Hero = SelectedHero;
+
+            return View();
+
+            //return Newtonsoft.Json.JsonConvert.SerializeObject(SelectedHero);
+        }
+    }
+}
---
+++ my-first-project/Models/Hero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace my_first_project.Models
+{
+    public class Hero
+    {
+        public String Name { get; set; }
+        public int Power { get; set; }
+        public Hero(string Name, int Power)
+        {
+            this.Name = Name;
+            this.Power = Power;
+        }
+
+    }
+}
